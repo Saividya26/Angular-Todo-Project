1e85f159af644ae8b2d9f508b5b1a04dc65e114a
Saividya26 <130060324+Saividya26@users.noreply.github.com>
2024-12-22 22:53:46 +0530

    todo list

---
+++ todo/todo/src/test/java/com/akshayacodes/todo/TodoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.akshayacodes.todo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+    @Disabled
+    void contextLoads() {
+    }
+
+}
